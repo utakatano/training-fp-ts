2d4d6928bb8b20cf690a23b69f58269a1d3d29fa
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-06-17 23:53:07 +0900

    create Array part2

--- app/training-fp-ts/src/fp-ts/Array.ts
+++ app/training-fp-ts/src/fp-ts/Array.ts
@@ -1,11 +1,45 @@
 import {
   takeLeft,
   takeLeftWhile,
-  takeRight
+  takeRight,
+  dropLeft,
+  dropLeftWhile,
+  dropRight,
+  foldLeft,
+  foldRight,
+  reduce,
+  reduceWithIndex
 } from 'fp-ts/es6/Array'
 
-// Array - takeLeft / takeLeftWhile / takeRight
 const cond = (n: number) => n < 3
+
+// Array - takeLeft / takeLeftWhile / takeRight
 console.log(takeLeft(2)([1, 2, 3, 4, 5]))
 console.log(takeRight(2)([1, 2, 3, 4, 5]))
 console.log(takeLeftWhile(cond)([1, 2, 3, 2, 1]))
+
+// Array - dropLeft / dropLeftWhile / dropRight
+console.log(dropLeft(2)([1, 2, 3, 4, 5]))
+console.log(dropRight(2)([1, 2, 3, 4, 5]))
+console.log(dropLeftWhile(cond)([1, 2, 3, 2, 1]))
+
+// Array - foldLeft / foldRight
+const foldLeftFunc: (as: number[]) => number = foldLeft(
+  () => 0,
+  (_n, tail) => tail.length
+)
+console.log(foldLeftFunc([1, 2, 3, 4, 5]))
+console.log(foldLeftFunc([]))
+
+const foldRightFunc: (as: number[]) => number = foldRight(
+  () => 0,
+  (_init, n) => n
+)
+console.log(foldRightFunc([]))
+console.log(foldRightFunc([1, 2, 3, 4, 5]))
+
+// Array - reduce / reduceWithIndex
+const reduceFunc = reduce(0, (acc, a: number) => acc + a)
+const reduceWithIndexFunc = reduceWithIndex('', (i, acc, a) => `${acc}${i}:${a}`)
+console.log(reduceFunc([1, 2, 3, 4, 5]))
+console.log(reduceWithIndexFunc(['foo', 'bar', 'baz']))
